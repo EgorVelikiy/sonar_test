532de1c4ef4d9ea836608130db96ecfe96e487ed
sarkiisov <sarkisov1337@gmail.com>
2025-04-08 10:42:01 +0300

    fix: replace "postgres" user with env var

--- db/init-db.sh
+++ db/init-db.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-psql -U postgres -c "CREATE DATABASE $POSTGRES_DB;"
+psql -U $POSTGRES_USER -c "CREATE DATABASE $POSTGRES_DB;"
 
-psql -U postgres -d $POSTGRES_DB < /tmp/init.sql
+psql -U $POSTGRES_USER -d $POSTGRES_DB < /tmp/init.sql
