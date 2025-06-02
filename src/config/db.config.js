194153fe86f25649bca920b7668e01b8fe888f15
Egor <egor.velikiy.02@mail.ru>
2025-06-02 20:17:43 +0300

    Revert "Trigger CI"
    This reverts commit 6b66317d9e4a0a74069cb1b4af56bb67c598c0b4.

---
+++ src/config/db.config.js
@@ -0,0 +1,20 @@
+require('dotenv').config()
+const { Pool, types } = require('pg')
+
+types.setTypeParser(1114, (value) => new Date(Date.parse(`${value}+0000`)));
+
+const pool = new Pool({
+  user: process.env.DB_USER,
+  host: process.env.DB_HOST,
+  database: process.env.DB_NAME,
+  password: process.env.DB_PASSWORD,
+  port: process.env.DB_PORT,
+});
+
+pool.query('SELECT NOW()', (err, res) => {
+  if (!err) {
+    console.log('Connected to the database:', res.rows);
+  }
+});
+
+module.exports = pool
